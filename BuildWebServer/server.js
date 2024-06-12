44df9597d506625438c1e84c1f0ebbe25ef2e643
Chinweike99 <innocentakwolu1@gmail.com>
2024-06-12 20:53:32 +0100

    installed npm

--- BuildWebServer/server.js
+++ BuildWebServer/server.js
@@ -12,6 +12,17 @@ const myEmmitter = new Emitter();
 const PORT = process.env.PORT || 3500;
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
+    let path;
+
+    if (req.url === '/' || req.url === 'index.html'){
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        path = path.join(__dirname, 'views', 'index.html');
+        fs.readFile(path, 'utf8', (err, data) => {
+            res.end(data);
+        })
+    }
+
 });
 
 server.listen(PORT, () => console.log(`server runinng on port ${PORT}`));
