d272e149b8485f3d49be9107d0d7441a5ee3c628
Chinweike99 <innocentakwolu1@gmail.com>
2024-06-14 03:52:20 +0100

    Middleware in Expressjs

--- Http_Requests/index.js
+++ Http_Requests/index.js
@@ -1,5 +1,9 @@
 import express from 'express';
-import path from 'path';
+import bodyParser from 'body-parser';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const app = express();
 const PORT = 3100;
 
@@ -7,12 +11,16 @@ const PORT = 3100;
 //     res.send("<h1>Welcome Tech bro</h1>");
 // })
 
+// Basically determines what is used behind then scenes to parse incoming data
+app.use(bodyParser.urlencoded({ extended: true}));
+
 app.get('/', (req, res) => {
-    // res.sendFile('./views/index.html', { root: __dirname });
-    // res.sendFile(path.join(__dirname, 'views', 'index.html'))
-    res.sendFile('./contact/contact.txt', { root: __dirname });
-    // res.sendFile(path.join(__dirname, 'contact', 'contact.txt'));
+    res.sendFile(__dirname + '/contact/index.html');
     // res.send(req.rawHeaders);
+})
+
+app.post("/submit", (req, res) => {
+    console.log(req.body);
 })
 
 app.listen(PORT, () => {
