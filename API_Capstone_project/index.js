7451c8c7f4f64c0d72695cdcb10aa1b9d7b4bb1f
Chinweike99 <innocentakwolu1@gmail.com>
2024-06-26 08:03:47 +0100

    Added styling

--- API_Capstone_project/index.js
+++ API_Capstone_project/index.js
@@ -5,6 +5,9 @@ const axios = require('axios');
 const app = express();
 const port = 3300;
 
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get('/', (req, res) => {
     res.render('index.ejs')
 });
