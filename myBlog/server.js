8f574a46fb7b5ce5b2c29279286dc51aa7791475
Chinweike99 <innocentakwolu1@gmail.com>
2024-07-01 08:54:47 +0100

    Added form to features.ejs

--- myBlog/server.js
+++ myBlog/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { default: axios } = require('axios');
 
 const app = express();
 const port = 3200;
@@ -8,9 +9,23 @@ const URL = "http://localhost:3300";
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.render("index.ejs",)
+// Main Page Route
+app.get('/', async (req, res) => {
+    try{
+        const response = await axios.get(URL + "/posts");
+        console.log(response);
+        res.render("index.ejs", {posts: response.data});
+    } catch (error) {
+        res.status(500).json({message: "Error occured"})
+    }
 });
+
+// Route rendering the Edit page
+app.get('/new', (req, res) => {
+    res.render("features.ejs", {heading: "Create Post", submit: "Add post"});
+})
+
+
 
 app.listen(port, () => {
     console.log(`Listening to PORT: ${port}`);
